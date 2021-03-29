0f4afe24be87016e8ef5b1ae8cf8205ca3f66eb6
Yanis <allseeingeye1315@gmail.com>
2021-03-29 18:27:20 +0300

    В файл HomeController.cs добавляем динамический объект ViewBag с значениями

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -10,6 +10,8 @@ namespace PartyInvites.Controllers
     {
         public ViewResult Index()
         {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Доброе утро" : "Доброго дня";
             return View();
         }
     }
