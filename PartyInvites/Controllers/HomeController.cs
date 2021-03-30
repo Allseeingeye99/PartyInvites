70a406a8d041414e83e8f29be75bbdf25a1aab05
Yanis <allseeingeye1315@gmail.com>
2021-03-30 17:40:16 +0300

    Добавляем методы  GET и POST, для обработки запросов  в класс HomeController

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PartyInvites.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,9 +15,18 @@ namespace PartyInvites.Controllers
             ViewBag.Greeting = hour < 12 ? "Доброе утро" : "Доброго дня";
             return View();
         }
+        [HttpGet]
         public ViewResult RsvpForm()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guest)
+        {
+            // Нужно отправить данные нового гостя no электронной почте 
+            // организатору вечеринки.
+            return View("Thanks", guest);
         }
     }
 }
